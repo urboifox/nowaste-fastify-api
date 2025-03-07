aabe60d07b3dd6f55c99d9ac3cd88f8bb6ef5474
Fox <urboifox@gmail.com>
2025-03-07 23:33:07 +0200

    seperate vercel deployment and local dev in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -6,40 +6,48 @@ import fastify, {
 import { routes } from './lib/routes';
 import { plugins } from './lib/plugins';
 
-// Fastify options
-const opts: FastifyServerOptions = {
-    logger: true
+const createServer = () => {
+    // Fastify options
+    const opts: FastifyServerOptions = {
+        logger: true
+    };
+
+    // Pretty logger
+    if (process.stdout.isTTY) {
+        opts.logger = {
+            transport: {
+                target: '@fastify/one-line-logger'
+            }
+        };
+    }
+
+    // Initialize app
+    const app = fastify(opts);
+
+    // Plugins
+    app.register(plugins);
+    app.register(routes, { prefix: '/api' });
+
+    app.get('/', () => {
+        return { hello: 'world' };
+    });
+
+    return app;
 };
 
-// Pretty logger
-if (process.stdout.isTTY) {
-    opts.logger = {
-        transport: {
-            target: '@fastify/one-line-logger'
-        }
-    };
+const app = createServer();
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+    try {
+        await app.listen({ port: 3000 });
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
 }
 
-// Initialize app
-const app = fastify(opts);
-
-// Plugins
-app.register(plugins);
-app.register(routes, { prefix: '/api' });
-
-app.get('/', () => {
-    return { hello: 'world' };
-});
-
-// Start listening for requests!
-try {
-    await app.listen({ port: 3000, host: '0.0.0.0' });
-} catch (err) {
-    app.log.error(err);
-    process.exit(1);
-}
-
-// For vercel deployment
+// For Vercel serverless deployment
 export default async function handler(req: FastifyRequest, res: FastifyReply) {
     await app.ready();
     app.server.emit('request', req, res);
